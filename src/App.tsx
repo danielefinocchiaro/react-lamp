2328cedfdbc6e73c643b25caeb9ea8b554f1764f
MineChannel5816 <danielefinocchiaro@keix.com>
2021-03-10 10:07:20 +0100

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,137 +1,174 @@
-import React, { useState, useReducer } from 'react';
-import arrayMove from 'array-move';
-import classNames from 'classnames';
+import React, { useState, useReducer, useEffect } from 'react';
 import _ from 'lodash/fp';
-
-import * as Icons from 'heroicons-react';
-
-import faker from 'faker';
+import classNames from 'classnames';
 
 interface P {}
 
-interface Button {
-  text: string;
-  onClick: () => void;
-}
-
-interface Data {
-  name: string;
-  favorite: boolean;
-}
-
-interface Element {
-  btnUp: Button;
-  btnDown: Button;
-  btnRemove: Button;
-  btnFavorite: () => void;
-  data: Data;
-  index: number;
-}
-
-function ButtonAction(props: Button) {
-  const style = classNames(
-    'm-1 cursor-pointer rounded p-1',
-    props.text === 'Remove' ? 'bg-red-300' : 'bg-green-300',
-  );
-  return (
-    <div className={style} onClick={props.onClick}>
-      {props.text}
-    </div>
-  );
-}
-
-function Element(props: Element) {
-  const styleElement = classNames(
-    'flex items-center mx-1',
-    props.data.favorite ? 'bg-yellow-200' : '',
-  );
-  console.log(props.data);
-  return (
-    <div className={styleElement}>
-      <div className="flex-grow">{props.data.name}</div>
-      <Icons.StarOutline
-        onClick={props.btnFavorite}
-        className="cursor-pointer"
-      />
-      <ButtonAction {...props.btnUp} />
-      <ButtonAction {...props.btnDown} />
-      <ButtonAction {...props.btnRemove} />
-    </div>
-  );
-}
-
-interface action {
-  type: string;
-  index: number;
-}
-
-function reducer(state: Data[], action: action) {
-  switch (action.type) {
-    case 'MoveUp':
-      if (action.index !== 0) {
-        return arrayMove(state, action.index, action.index - 1);
-      }
-      break;
-    case 'MoveDown':
-      if (action.index !== state.length) {
-        return arrayMove(state, action.index, action.index + 1);
-      }
-      break;
-    case 'Remove':
-      return state.filter((p, i) => i != action.index);
-    case 'Add':
-      let person = { name: faker.name.findName(), favorite: false };
-      let newState = [...state, person];
-      return newState;
-    case 'Favorite':
-      return _.update(
-        `${action.index}.favorite`,
-        (prev: boolean) => !prev,
-        state,
-      );
-  }
-  return state;
-}
-
 function App(props: P) {
-  let peopleArr = new Array(20).fill({}).map(() => ({
-    name: faker.name.findName(),
-    favorite: false,
-  }));
+  const [timer, setTimer] = useState(0);
+  const [pause, setPause] = useState(true);
+  const [stop, setStop] = useState(false);
+  const [laps, setLaps] = useState<string[]>([]);
+  const [state, setState] = useState('stop');
 
-  const [people, dispatch] = useReducer(reducer, peopleArr);
+  interface Button {
+    onClick: () => void;
+    text?: string;
+  }
 
-  return (
-    <div className="flex flex-col">
-      {people.map((p: Data, i: number) => {
-        return (
-          <Element
-            data={p}
-            index={i}
-            btnFavorite={() => dispatch({ type: 'Favorite', index: i })}
-            btnUp={{
-              text: 'Up',
-              onClick: () => dispatch({ type: 'MoveUp', index: i }),
-            }}
-            btnDown={{
-              text: 'Down',
-              onClick: () => dispatch({ type: 'MoveDown', index: i }),
-            }}
-            btnRemove={{
-              text: 'Remove',
-              onClick: () => dispatch({ type: 'Remove', index: i }),
-            }}
-            key={i}
-          />
-        );
-      })}
-      <div
-        className="m-1 cursor-pointer bg-blue-300 rounded p-1"
-        onClick={() => dispatch({ type: 'Add', index: 0 })}
+  function newLap() {
+    let str = `Lap ${laps.length}: ${timer} seconds!`;
+    const newLaps = [...laps, str];
+    setLaps(newLaps);
+    setTimer(0);
+  }
+
+  function BtnStart(p: Button) {
+    const style = classNames(
+      'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-green-500',
+    );
+
+    return (
+      <button
+        className={style}
+        disabled={state === 'start' ? true : false}
+        onClick={p.onClick}
       >
-        Add
+        {p.text}
+      </button>
+    );
+  }
+
+  function BtnStop(p: Button) {
+    const style = classNames(
+      'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-red-500',
+    );
+
+    return (
+      <button
+        className={style}
+        disabled={state === 'stop' ? true : false}
+        onClick={p.onClick}
+      >
+        Stop
+      </button>
+    );
+  }
+
+  function BtnPause(p: Button) {
+    const style = classNames(
+      'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-blue-500',
+    );
+    console.log(p.text);
+    return (
+      <button
+        className={style}
+        disabled={state === 'stop' ? true : false}
+        onClick={p.onClick}
+      >
+        {p.text}
+      </button>
+    );
+  }
+
+  function BtnReset(p: Button) {
+    const style = classNames(
+      'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-blue-500',
+    );
+
+    return (
+      <button
+        className={style}
+        disabled={state === 'stop' ? true : false}
+        onClick={p.onClick}
+      >
+        {p.text}
+      </button>
+    );
+  }
+
+  function BtnLap(p: Button) {
+    const style = classNames(
+      'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed',
+      p.text === 'Stop' ? 'bg-red-500' : 'bg-yellow-500',
+    );
+
+    return (
+      <button
+        className={style}
+        disabled={state === 'stop' ? true : false}
+        onClick={p.onClick}
+      >
+        {p.text}
+      </button>
+    );
+  }
+
+  useEffect(() => {
+    console.log(state === 'stop' ? true : false);
+    if (pause) {
+      return;
+    }
+
+    if (stop) {
+      setTimer(0);
+    }
+
+    const counter = setInterval(() => {
+      setTimer((s) => s + 1);
+    }, 1000);
+
+    return () => clearInterval(counter);
+  }, [pause]);
+
+  const [pauseEffect, setPauseEffect] = useState('Pause');
+  return (
+    <>
+      <div className="flex m-4">
+        <BtnStart
+          text="Start"
+          onClick={() => {
+            setPause((p) => !p);
+            setState('start');
+          }}
+        />
+        <BtnStop
+          text="Stop"
+          onClick={() => {
+            setPause((p) => !p);
+            setStop((s) => !s);
+            setState('stop');
+          }}
+        />
+        <BtnPause
+          onClick={() => {
+            setPause((p) => !p);
+            if (pauseEffect === 'Pause') {
+              setPauseEffect('Play');
+            } else {
+              setPauseEffect('Pause');
+            }
+          }}
+          text={pauseEffect}
+        />
+        <BtnReset
+          text="Reset"
+          onClick={() => {
+            setPause(true);
+            setTimer(0);
+            setState('stop');
+          }}
+        />
+        <BtnLap text="Lap" onClick={() => newLap()} />
       </div>
-    </div>
+      <div className="p-4">Timer: {timer} seconds!</div>
+      <div>LAP:</div>
+      <div>
+        {laps.map((lap, i) => {
+          return <div key={i}>{lap}</div>;
+        })}
+      </div>
+    </>
   );
 }
 
