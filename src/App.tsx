ae1f2f144aa090d1a362e16dcea3103281128c88
MineChannel5816 <danielefinocchiaro@keix.com>
2021-03-19 13:29:37 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,201 +7,124 @@ import React, {
 } from 'react';
 import _ from 'lodash/fp';
 import classNames from 'classnames';
-import type { Dispatch } from 'react';
 
-interface State {
-  status: 'stopped' | 'started' | 'paused';
-  seconds: number;
-  laps: number[];
+interface Event {
+  status: 'LAMPADINA_SPENTA' | 'LAMPADINA_ACCESA';
+  timestamp: Date;
 }
 
-interface Action {
-  type: 'START' | 'STOP' | 'PAUSE' | 'RESET' | 'LAP' | 'UPDATE';
-  payload?: {};
-}
+function App() {
+  const [events, setEvents] = useState<Event[]>([
+    { status: 'LAMPADINA_SPENTA', timestamp: new Date() },
+  ]);
+  const [seconds, setSeconds] = useState<number>(0);
 
-interface Context {
-  state: State;
-  dispatch: Dispatch<Action>;
-}
+  function BtnOn(p: Event[]) {
+    const style = classNames(
+      'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-green-500',
+    );
 
-function useMyCustomHook() {
-  const reducerFunction = useCallback(
-    (state: State, payload: Action): State => {
-      switch (payload.type) {
-        case 'START':
-          if (state.status === 'stopped') {
-            return { ...state, seconds: 0, status: 'started' };
-          } else {
-            return { ...state, seconds: state.seconds, status: 'started' };
-          }
-        case 'STOP':
-          return { ...state, seconds: state.seconds, status: 'stopped' };
-        case 'PAUSE':
-          if (state.status === 'paused') {
-            return { ...state, status: 'started' };
-          } else {
-            return { ...state, status: 'paused' };
-          }
-        case 'RESET':
-          return { ...state, status: 'stopped', laps: [], seconds: 0 };
-        case 'LAP':
-          return { ...state, laps: [...state.laps, state.seconds], seconds: 0 };
-        case 'UPDATE':
-          return { ...state, seconds: state.seconds + 1 };
+    return (
+      <button
+        className={style}
+        //disabled={state.status === 'started' ? true : false}
+        onClick={() => setLightOn(p)}
+      >
+        Acceso
+      </button>
+    );
+  }
+
+  function BtnOff(p: Event[]) {
+    const style = classNames(
+      'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-red-500',
+    );
+
+    return (
+      <button
+        className={style}
+        //disabled={state.status === 'stopped' ? true : false}
+        onClick={() => setLightOff(p)}
+      >
+        Spento
+      </button>
+    );
+  }
+
+  function Lampada(p: Event[]) {
+    let actualStatus = p[Object.keys(p).length - 1];
+    const style = classNames(
+      'flex flex-grow justify-center h-20',
+      actualStatus.status === 'LAMPADINA_ACCESA'
+        ? 'bg-yellow-500'
+        : 'bg-gray-500',
+    );
+    return <div className={style}>{actualStatus.status}</div>;
+  }
+
+  const setLightOn = useCallback(
+    (p: Event[]) => {
+      let actualStatus = p[Object.keys(p).length - 1];
+      if (actualStatus.status === 'LAMPADINA_SPENTA') {
+        setEvents([
+          ...events,
+          { status: 'LAMPADINA_ACCESA', timestamp: new Date() },
+        ]);
       }
     },
-    [],
+    [events],
   );
-  const [state, dispatch] = useReducer(reducerFunction, {
-    status: 'stopped',
-    seconds: 0,
-    laps: new Array().fill(0),
-  });
+
+  const setLightOff = useCallback(
+    (p: Event[]) => {
+      let actualStatus = p[Object.keys(p).length - 1];
+      if (actualStatus.status === 'LAMPADINA_ACCESA') {
+        setEvents([
+          ...events,
+          { status: 'LAMPADINA_SPENTA', timestamp: new Date() },
+        ]);
+      }
+    },
+    [events],
+  );
+
+  function isLastMinute(event: Event) {
+    let actualDate = new Date();
+    if (event.timestamp.getTime() > actualDate.getTime() - 60000) return event;
+  }
+
+  function Counter(p: Event[]) {
+    return <div>Seconds in the last minute: {seconds}</div>;
+  }
 
   useEffect(() => {
-    if (state.status === 'paused') {
-      return;
-    }
-
-    if (state.status === 'stopped') {
-      return;
-    }
-
     const counter = setInterval(() => {
-      dispatch({ type: 'UPDATE' });
+      let lastMinute = events.filter(isLastMinute);
+      console.log(lastMinute);
+      let actualDate = new Date();
+      let test = actualDate.g;
+      console.log(test);
+      setSeconds();
     }, 1000);
 
     return () => clearInterval(counter);
-  }, [state.status]);
+  }, [events]);
 
-  return {
-    state,
-    dispatch,
-  };
-}
-
-function BtnLap() {
-  const { state, dispatch } = useContext(StateContext);
-  const style = classNames(
-    'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed',
-    'bg-yellow-500',
-  );
-
-  return (
-    <button
-      className={style}
-      disabled={state.status === 'stopped' ? true : false}
-      onClick={() => dispatch({ type: 'LAP' })}
-    >
-      Lap
-    </button>
-  );
-}
-function BtnStart() {
-  const { state, dispatch } = useContext(StateContext);
-  const style = classNames(
-    'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-green-500',
-  );
-
-  return (
-    <button
-      className={style}
-      disabled={state.status === 'started' ? true : false}
-      onClick={() => dispatch({ type: 'START' })}
-    >
-      Start
-    </button>
-  );
-}
-
-function BtnStop() {
-  const { state, dispatch } = useContext(StateContext);
-
-  const style = classNames(
-    'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-red-500',
-  );
-
-  return (
-    <button
-      className={style}
-      disabled={state.status === 'stopped' ? true : false}
-      onClick={() => dispatch({ type: 'STOP' })}
-    >
-      Stop
-    </button>
-  );
-}
-
-function BtnPause() {
-  const { state, dispatch } = useContext(StateContext);
-
-  const style = classNames(
-    'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-blue-500',
-  );
-
-  return (
-    <button
-      className={style}
-      disabled={state.status === 'stopped' ? true : false}
-      onClick={() => dispatch({ type: 'PAUSE' })}
-    >
-      {state.status === 'paused' ? 'Play' : 'Pause'}
-    </button>
-  );
-}
-
-function BtnReset() {
-  const { state, dispatch } = useContext(StateContext);
-
-  const style = classNames(
-    'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-blue-500',
-  );
-
-  return (
-    <button
-      className={style}
-      disabled={state.status === 'stopped' ? true : false}
-      onClick={() => dispatch({ type: 'RESET' })}
-    >
-      Reset
-    </button>
-  );
-}
-
-const StateContext = React.createContext<Context>({
-  state: {
-    status: 'stopped',
-    seconds: 0,
-    laps: new Array().fill(0),
-  },
-  dispatch: () => {},
-});
-
-function App() {
-  const { state, dispatch } = useMyCustomHook();
   return (
     <>
-      <StateContext.Provider value={{ state, dispatch }}>
-        <div className="flex m-4">
-          <BtnStart />
-          <BtnStop />
-          <BtnPause />
-          <BtnReset />
-          <BtnLap />
-        </div>
-        <div className="p-4">Timer: {state.seconds} seconds!</div>
-        <div>LAP:</div>
-        <div>
-          {state.laps.map((lap, i) => {
-            return (
-              <div key={i}>
-                Lap {i + 1}: {lap} seconds!
-              </div>
-            );
-          })}
-        </div>
-      </StateContext.Provider>
+      <div className="flex-grow justify-center flex m-4">
+        <BtnOn {...events} />
+        <BtnOff {...events} />
+        <button
+          onClick={() => {
+            console.log(events);
+          }}
+        >
+          aaa
+        </button>
+      </div>
+      <Lampada {...events} />
+      <Counter {...events} />
     </>
   );
 }
